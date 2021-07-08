2f64c4a45828407ea189fd8f90ffc962c7b3a477
nessessary <wangkang2010@163.com>
2021-07-08 11:14:36 +0800

    env root_path

--- autoxd/cnn_boll/env.py
+++ autoxd/cnn_boll/env.py
@@ -2,7 +2,7 @@
 
 """配置环境"""
 import os
-from autoxd import myredis
+from autoxd import myredis,agl
 
 class enum:
     ROOT_PATH = 'root_path'
@@ -22,8 +22,18 @@ def get_root_path():
     if enum.use_redis:
         root_path = myredis.get_obj(key)
     else:
-        cur_path = os.path.abspath(__file__)
-        cur_path = os.path.dirname(cur_path)
-        root_path = os.path.abspath(os.path.join(cur_path, '..'))
+        cur_path = agl.get_exec_file_path()
+        key_path = "autoxd\\autoxd"
+        pos = cur_path.find(key_path)
+        if pos <=0 :
+            key_path = "autoxd/autoxd"
+            pos = cur_path.find(key_path)
+        root_path = cur_path[:pos+len(key_path)]
+        #cur_path = os.path.abspath(__file__)
+        #cur_path = os.path.dirname(cur_path)
+        #root_path = os.path.abspath(os.path.join(cur_path, '..'))
     assert(os.path.isdir(root_path))
     return root_path
+
+if __name__ == "__main__":
+    print(get_root_path())
